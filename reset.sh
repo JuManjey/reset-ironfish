dd7e02ff1365b7d0ae295bf06981c4ec7f9147a2
JuManji <opghld@gmail.com>
2022-05-18 17:12:08 +0300

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-. $HOME/.bashrc
-. $HOME/.bash_profile
+
 mkdir -p ~/ironfish_keys
-ironfish accounts:export $IRONFISH_WALLET ~/ironfish_keys/$IRONFISH_WALLET.json
+yarn --cwd ~/ironfish/ironfish-cli/ start accounts:export $IRONFISH_WALLET ~/ironfish_keys/$IRONFISH_WALLET.json
 cat  ~/ironfish_keys/$IRONFISH_WALLET.json
 systemctl stop ironfishd ironfishd-miner
-ironfish reset
-ironfish accounts:import ~/ironfish_keys/$IRONFISH_WALLET.json
+yarn --cwd ~/ironfish/ironfish-cli/ start reset
+yarn --cwd ~/ironfish/ironfish-cli/ start accounts:import ~/ironfish_keys/$IRONFISH_WALLET.json
 systemctl restart ironfishd ironfishd-miner
-ironfish config:get blockGraffiti
 systemctl status ironfishd ironfishd-miner
 
 
